115fd06552f1baba9d73a09ca1c2ca8f807ef985
Jeroen <dijkvan@gmail.com>
2019-02-06 17:27:07 +0100

    POST method for study (#4)

--- src/routes/studies.php
+++ src/routes/studies.php
@@ -78,5 +78,32 @@ $app->get('/json', function (Request $request, Response $response, array $args) 
                     ->withHeader('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
                     ->withHeader('Pragma', 'public');
 })->setName('json');
+    
+// Add a study
+$app->post('/study', function (Request $request, Response $response) {
+    $data = $request->getParsedBody();
+    
+    $sql = "INSERT INTO study (author_id, week_id, day_no, publish_date, title, passage_ref, passage_text, prayer_intro, body, prayer_outro, audio_url, video_url)
+    VALUES (:author_id, :week_id, :day_no, :publish_date, :title, :passage_ref, :passage_text, :prayer_intro, :body, :prayer_outro, :audio_url, :video_url)";
+    
+    $stmt = $this->db->prepare($sql);
+    
+    $stmt->bindParam("author_id", $data['author_id']);
+    $stmt->bindParam("week_id", $data['week_id']);
+    $stmt->bindParam("day_no", $data['day_no']);
+    $stmt->bindParam("publish_date", $data['publish_date']);
+    $stmt->bindParam("title", $data['title']);
+    $stmt->bindParam("passage_ref", $data['passage_ref']);
+    $stmt->bindParam("passage_text", $data['passage_text']);
+    $stmt->bindParam("prayer_intro", $data['prayer_intro']);
+    $stmt->bindParam("body", $data['body']);
+    $stmt->bindParam("prayer_outro", $data['prayer_outro']);
+    $stmt->bindParam("audio_url", $data['audio_url']);
+    $stmt->bindParam("video_url", $data['video_url']);
+    
+    $stmt->execute();
+    
+    });
+
 
 ?>
